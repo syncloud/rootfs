42677f852eb800486ef39dd54e12294d8e8ea945
Boris Rybalkin <ribalkin@gmail.com>
2018-02-17 19:48:17 +0000

    try machinectl

--- rootfs.sh
+++ rootfs.sh
@@ -71,7 +71,9 @@ echo "configuring rootfs"
 cp installer_$INSTALLER.sh ${ROOTFS}/root/installer.sh
 nohup systemd-nspawn --network-veth -bD ${ROOTFS} &
 sleep 20
-systemd-run --wait --pty --machine=rootfs /bin/bash -c "/root/installer.sh ${RELEASE} ${POINT_TO_RELEASE}"
+#systemd-run --wait --pty --machine=rootfs /bin/bash -c "/root/installer.sh ${RELEASE} ${POINT_TO_RELEASE}"
+machinectl shell rootfs /bin/bash -c "/root/installer.sh ${RELEASE} ${POINT_TO_RELEASE}"
+
 rm ${ROOTFS}/root/installer.sh
 
 #umount ${ROOTFS}/dev/pts
