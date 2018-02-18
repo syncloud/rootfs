d36b1676c0853816269a6ca07ba96b671bc1e6ef
Boris Rybalkin <ribalkin@gmail.com>
2018-02-18 21:27:32 +0000

    try host network

--- rootfs.sh
+++ rootfs.sh
@@ -2,13 +2,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 
-#Fix debconf frontend warnings
-#export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-export DEBCONF_FRONTEND=noninteractive
-export DEBIAN_FRONTEND=noninteractive
-export TMPDIR=/tmp
-export TMP=/tmp
-
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
@@ -25,20 +18,6 @@ POINT_TO_RELEASE=$2
 INSTALLER=$3
 
 BASE_ROOTFS_ZIP=rootfs-${ARCH}.tar.gz
-ROOTFS=${DIR}/rootfs
-
-function cleanup {
-    mount | grep $ROOTFS
-    mount | grep $ROOTFS | awk '{print "umounting "$1; system("umount "$3)}'
-    mount | grep $ROOTFS
-
-    echo "killing chroot services"
-    lsof 2>&1 | grep $ROOTFS | grep -v docker | grep -v rootfs.sh | awk '{print $1" "$2}' | sort | uniq
-
-    #lsof 2>&1 | grep $ROOTFS | grep -v docker | grep -v rootfs.sh | awk '{print $2}' | sort | uniq | xargs kill -9
-
-    lsof 2>&1 | grep $ROOTFS
-}
 
 ls -la
 
@@ -49,34 +28,11 @@ else
   echo "rootfs is found"
 fi
 
-cleanup || true
-
-rm -rf ${ROOTFS}
-mkdir -p ${ROOTFS}
-
-echo "extracting rootfs"
-
-#tar xzf ${BASE_ROOTFS_ZIP} -C ${ROOTFS}
-#rm -rf ${BASE_ROOTFS_ZIP}
-
-#echo "disable service restart"
-#cp disable-service-restart.sh ${ROOTFS}/root
-#chroot ${ROOTFS} /root/disable-service-restart.sh
-
-echo "configuring rootfs"
-#mount -v --bind /dev ${ROOTFS}/dev
-#chroot ${ROOTFS} /bin/bash -c "mount -t devpts devpts /dev/pts"
-#chroot ${ROOTFS} /bin/bash -c "mount -t proc proc /proc"
-
 docker kill rootfs || true
 docker rm rootfs || true
 docker rmi rootfs || true
 docker import ${BASE_ROOTFS_ZIP} rootfs
 docker run -d --privileged -i -p 2222:22 --name rootfs rootfs /sbin/init
-
-#nohup systemd-nspawn --network-veth -bD ${ROOTFS} -p 2222:22 &
-#sleep 60
-#systemd-run --wait --pty --machine=rootfs /bin/bash -c "/root/installer.sh ${RELEASE} ${POINT_TO_RELEASE}"
 
 attempts=100
 attempt=0
@@ -95,27 +51,11 @@ do
 done
 set -e
 
-cp installer_$INSTALLER.sh ${ROOTFS}/root/installer.sh
-
-sshpass -p syncloud ssh -o StrictHostKeyChecking=no -p 2222 root@localhost /bin/bash -c /root/installer.sh ${RELEASE} ${POINT_TO_RELEASE}"
-
-#machinectl -h
-#machinectl --version
-#machinectl shell rootfs "/bin/bash -c /root/installer.sh ${RELEASE} ${POINT_TO_RELEASE}"
-
-rm ${ROOTFS}/root/installer.sh
-
-#umount ${ROOTFS}/dev/pts
-#umount ${ROOTFS}/dev
-#umount ${ROOTFS}/proc
-rm -rf ${ROOTFS}/tmp/*
-
-cleanup || true
-
-#echo "enable restart"
-#cp enable-service-restart.sh ${ROOTFS}/root
-#chroot ${ROOTFS} /root/enable-service-restart.sh
-
-rm -rf syncloud-rootfs-${ARCH}-${INSTALLER}.tar.gz
-tar czf syncloud-rootfs-${ARCH}-${INSTALLER}.tar.gz -C ${ROOTFS} .
-rm -rf ${ROOTFS}
+sshpass -p syncloud scp -o StrictHostKeyChecking=no installer_${INSTALLER}.sh root@localhost:/root/installer.sh
+sshpass -p syncloud ssh -o StrictHostKeyChecking=no -p 2222 root@localhost /root/installer.sh ${RELEASE} ${POINT_TO_RELEASE}
+sshpass -p syncloud ssh -o StrictHostKeyChecking=no -p 2222 root@localhost rm /root/installer.sh
+sshpass -p syncloud ssh -o StrictHostKeyChecking=no -p 2222 root@localhost rm -rf /tmp/*
+docker kill rootfs
+docker export rootfs | gzip > syncloud-rootfs-${ARCH}-${INSTALLER}.tar.gz
+docer rm rootfs
+docer rmi rootfs
