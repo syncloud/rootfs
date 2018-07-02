1c9892a9304b4fcfb1736d41a7e4cbe1b4c3f58f
Boris Rybalkin <ribalkin@gmail.com>
2018-07-02 12:00:32 +0100

    fix debian arch

--- rootfs.sh
+++ rootfs.sh
@@ -16,6 +16,7 @@ RELEASE=$1
 POINT_TO_RELEASE=$2
 INSTALLER=$3
 ARCH=$4
+DEBIAN_ARCH=$(dpkg --print-architecture)
 
 BASE_ROOTFS_ZIP=rootfs-${ARCH}.tar.gz
 
@@ -66,7 +67,7 @@ rm -rf rootfs
 mkdir rootfs
 tar xzf docker-rootfs-${ARCH}-${INSTALLER}.tar.gz -C rootfs
 rm -rf docker-rootfs-${ARCH}-${INSTALLER}.tar.gz
-cp ${DIR}/bootstrap/${ARCH}/etc/hosts rootfs/etc/hosts
+cp ${DIR}/bootstrap/${DEBIAN_ARCH}/etc/hosts rootfs/etc/hosts
 cat rootfs/etc/hosts
 tar czf syncloud-rootfs-${ARCH}-${INSTALLER}.tar.gz -C rootfs .
 rm -rf rootfs
