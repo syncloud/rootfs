1973385a7abb066df7751ccda31da217f3fee4c4
Boris Rybalkin <ribalkin@gmail.com>
2018-11-01 04:22:10 +0100

    test on standard debian stretch

--- integration/test-docker.sh
+++ integration/test-docker.sh
@@ -3,20 +3,21 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
-if [[ -z "$6" ]]; then
-    echo "usage $0 redirect_user redirect_password redirect_domain release device_host installer"
+if [[ -z "$5" ]]; then
+    echo "usage $0 redirect_user redirect_password redirect_domain release device_host user password"
     exit 1
 fi
 
 RELEASE=$4
 DEVICE_HOST=$5
-INSTALLER=$6
+USER=$6
+PASSWORD=$7
 
 attempts=100
 attempt=0
 
 set +e
-sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@${DEVICE_HOST} date
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@${DEVICE_HOST} date
 while test $? -gt 0
 do
   if [ $attempt -gt $attempts ]; then
@@ -25,13 +26,13 @@ do
   sleep 3
   echo "Waiting for SSH $attempt"
   attempt=$((attempt+1))
-  sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@${DEVICE_HOST} date
+  sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@${DEVICE_HOST} date
 done
 set -e
 
-sshpass -p syncloud scp -o StrictHostKeyChecking=no install-${INSTALLER}.sh root@${DEVICE_HOST}:/installer.sh
+sshpass -p $PASSWORD scp -o StrictHostKeyChecking=no install.sh $USER@${DEVICE_HOST}:/install.sh
 
-sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@${DEVICE_HOST} /installer.sh ${INSTALLER_VERSION} ${RELEASE}
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@${DEVICE_HOST} sudo /install.sh
 
 pip2 install -r ${DIR}/dev_requirements.txt
 pip2 install -U pytest
