9adedb6de01594a411362797a732c8c4998c1f4d
Boris Rybalkin <ribalkin@gmail.com>
2018-11-13 06:51:08 +0000

    fix stretch test

--- integration/test-docker.sh
+++ integration/test-docker.sh
@@ -3,15 +3,17 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
-if [[ -z "$3" ]]; then
-    echo "usage $0 redirect_user redirect_password device_host"
+if [[ -z "$1" ]]; then
+    echo "usage $0 device_host"
     exit 1
 fi
 
-DEVICE_HOST=$3
+REDIRECT_USER=teamcity@syncloud.it
+REDIRECT_PASSWORD=password
+DEVICE_HOST=$1
 DOMAIN=$DEVICE_HOST-${ARCH}-${DRONE_BRANCH}
 
 pip2 install -r ${DIR}/dev_requirements.txt
 pip2 install -U pytest
 
-py.test -sx verify.py --email=$1 --password=$2 --domain=$DOMAIN --device-host=$DEVICE_HOST
+py.test -sx verify.py --email=$REDIRECT_USER --password=$REDIRECT_PASSWORD --domain=$DOMAIN --device-host=$DEVICE_HOST
