195b69f99703312fca1026fb1821e94aeef37356
Boris Rybalkin <ribalkin@gmail.com>
2019-04-22 21:11:41 +0100

    fix pytest conftest

--- rootfs.sh
+++ rootfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd $DIR
+cd ${DIR}
 
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
@@ -22,13 +22,6 @@ BOOTSTRAP_ROOTFS_ZIP=bootstrap-${ARCH}.tar.gz
 
 ls -la
 
-if [[ ! -f ${BOOTSTRAP_ROOTFS_ZIP} ]]; then
-  echo "${BOOTSTRAP_ROOTFS_ZIP} is not found"
-  wget http://artifact.syncloud.org/image/${BASE_ROOTFS_ZIP} --progress dot:giga
-else
-  echo "bootstrap rootfs is found"
-fi
-
 docker kill rootfs || true
 docker rm rootfs || true
 docker rmi rootfs || true
@@ -36,20 +29,21 @@ docker import ${BOOTSTRAP_ROOTFS_ZIP} rootfs
 docker run -d --privileged -i --name rootfs rootfs /sbin/init
 device_host=$(docker container inspect --format '{{ .NetworkSettings.IPAddress }}' rootfs)
 
-./integration/wait-ssh.sh $device_host root syncloud 22
+./integration/wait-ssh.sh ${device_host} root syncloud 22
 
-sshpass -p syncloud scp -o StrictHostKeyChecking=no install.sh root@$device_host:/root/install.sh
+sshpass -p syncloud scp -o StrictHostKeyChecking=no install.sh root@${device_host}:/root/install.sh
 DOCKER_RUN="sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@$device_host"
-$DOCKER_RUN /root/install.sh ${RELEASE} ${POINT_TO_RELEASE}
-$DOCKER_RUN rm /root/install.sh
-$DOCKER_RUN rm -rf /tmp/*
+${DOCKER_RUN} /root/install.sh ${RELEASE} ${POINT_TO_RELEASE}
+${DOCKER_RUN} rm /root/install.sh
+${DOCKER_RUN} rm -rf /tmp/*
 
 docker export rootfs | gzip > docker-rootfs-${ARCH}.tar.gz
 
 #test
 pip2 install -r ${DIR}/dev_requirements.txt
+cd integration
 py.test -sx verify.py --domain=${DOMAIN} --device-host=${device_host} --device-port=22
-
+cd ${DIR}
 docker kill rootfs
 docker rm rootfs
 docker rmi rootfs
