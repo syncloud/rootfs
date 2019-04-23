39f77d95ad104aeaf7e3e92f16a12ee984f32c6a
Boris Rybalkin <ribalkin@gmail.com>
2019-04-23 07:29:08 +0100

    fix session scope

--- integration/verify.py
+++ integration/verify.py
@@ -36,11 +36,6 @@ def copy_logs(device, app, log_dir):
     device.scp_from_device(device_logs, app_log_dir)
 
 
-@pytest.fixture(scope='session')
-def syncloud_session(device):
-    return device.login()
-
-
 def test_start(module_setup, log_dir):
     shutil.rmtree(log_dir, ignore_errors=True)
 
@@ -50,13 +45,13 @@ def test_activate_device(device):
     assert response.status_code == 200, response.text
 
 
-def wait_for_app(device_host, syncloud_session, predicate):
+def wait_for_app(device, predicate):
     found = False
     attempts = 10
     attempt = 0
     while not found and attempt < attempts:
         try:
-            response = syncloud_session.get('http://{0}/rest/installed_apps'.format(device_host))
+            response = device.login().get('http://{0}/rest/installed_apps'.format(device.device_host))
             if response.status_code == 200:
                 print('result: {0}'.format(response.text))
                 found = predicate(response.text)
@@ -67,24 +62,22 @@ def wait_for_app(device_host, syncloud_session, predicate):
         time.sleep(5)
 
 
-def test_login(device):
-    return device.login()
-
-
 @pytest.mark.parametrize("app", APPS)
-def test_app_install(syncloud_session, app, device_host):
-    response = syncloud_session.get('https://{0}/rest/install?app_id={1}'.format(device_host, app),
+def test_app_install(device, app):
+    syncloud_session = device.login()
+    response = syncloud_session.get('https://{0}/rest/install?app_id={1}'.format(device.device_host, app),
                                     allow_redirects=False, verify=False)
 
     assert response.status_code == 200
-    wait_for_installer(syncloud_session, device_host)
-    wait_for_app(device_host, syncloud_session, lambda response_text: app in response_text)
+    wait_for_installer(syncloud_session, device.device_host)
+    wait_for_app(device, lambda response_text: app in response_text)
 
 
 @pytest.mark.parametrize("app", APPS)
-def test_app_remove(syncloud_session, app, device_host):
-    response = syncloud_session.get('https://{0}/rest/remove?app_id={1}'.format(device_host, app),
+def test_app_remove(device, app):
+    syncloud_session = device.login()
+    response = syncloud_session.get('https://{0}/rest/remove?app_id={1}'.format(device.device_host, app),
                                     allow_redirects=False, verify=False)
     assert response.status_code == 200
-    wait_for_installer(syncloud_session, device_host)
-    wait_for_app(device_host, syncloud_session, lambda response_text: app not in response_text)
+    wait_for_installer(syncloud_session, device.device_host)
+    wait_for_app(device, lambda response_text: app not in response_text)
