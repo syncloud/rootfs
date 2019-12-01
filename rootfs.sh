5a13cd46fec8d84c627b0d8bec48bc040047efab
Boris Rybalkin <ribalkin@gmail.com>
2019-12-01 13:01:37 +0000

    fix test docker host

--- rootfs.sh
+++ rootfs.sh
@@ -19,32 +19,31 @@ DOMAIN=${ARCH}-${DRONE_BRANCH}
 BOOTSTRAP_ROOTFS_ZIP=bootstrap-${ARCH}.tar.gz
 
 ls -la
-device_host=rootfsvm
-docker kill rootfs || true
-docker rm rootfs || true
-docker rmi rootfs || true
-docker import ${BOOTSTRAP_ROOTFS_ZIP} rootfs
-docker run -d --privileged -i --name rootfs --hostname ${device_host} --network=drone rootfs /sbin/init
-#device_host=$(docker container inspect --format '{{ .NetworkSettings.IPAddress }}' rootfs)
+device=rootfsvm
+docker kill ${device} || true
+docker rm ${device} || true
+docker rmi ${device} || true
+docker import ${BOOTSTRAP_ROOTFS_ZIP} ${device}
+docker run -d --privileged -i --name ${device} --hostname ${device} --network=drone ${device} /sbin/init
 
-./integration/wait-ssh.sh ${device_host} root syncloud 22
+./integration/wait-ssh.sh ${device} root syncloud 22
 
-sshpass -p syncloud scp -o StrictHostKeyChecking=no install.sh root@${device_host}:/root/install.sh
-DOCKER_RUN="sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@$device_host"
+sshpass -p syncloud scp -o StrictHostKeyChecking=no install.sh root@${device}:/root/install.sh
+DOCKER_RUN="sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@$device"
 ${DOCKER_RUN} /root/install.sh
 ${DOCKER_RUN} rm /root/install.sh
 ${DOCKER_RUN} rm -rf /tmp/*
 
-docker export rootfs | gzip > docker-rootfs-${ARCH}.tar.gz
+docker export ${device} | gzip > docker-rootfs-${ARCH}.tar.gz
 
 #test
 pip2 install -r ${DIR}/dev_requirements.txt
 cd integration
-py.test -sx verify.py --domain=${DOMAIN} --device-host=${device_host}
+py.test -sx verify.py --domain=${DOMAIN} --device-host=${device}
 cd ${DIR}
-docker kill rootfs
-docker rm rootfs
-docker rmi rootfs
+docker kill ${device}
+docker rm ${device}
+docker rmi ${device}
 
 rm -rf rootfs
 mkdir rootfs
