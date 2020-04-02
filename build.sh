e79e57ba892525895abef1477606d176e5711b03
Boris Rybalkin <ribalkin@gmail.com>
2020-04-02 13:21:48 +0100

    remove inet6 from jessie

--- build.sh
+++ build.sh
@@ -52,9 +52,10 @@ mkdir rootfs
 tar xf docker-rootfs.tar -C rootfs
 mkdir log
 ls -la rootfs/var/log > log/files.log
-cp rootfs/var/log/messages log/messages.log
-cp rootfs/var/log/auth.log log
-cp rootfs/var/log/syslog log/syslog.log
+cp rootfs/var/log/messages log/messages.log | ture
+cp rootfs/var/log/auth.log log | ture
+cp rootfs/var/log/syslog log/syslog.log | ture
+cp rootfs/var/log/dmesg log/dmesg.log | ture
 chmod -R a+r log
 rsync -avh --stats bootstrap/files/common/ rootfs
 rsync -avh --stats bootstrap/files/arch/${DEBIAN_ARCH}/ rootfs
