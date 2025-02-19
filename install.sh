6416221270025ec42160acf9be3f4c9988c44a4a
Boris Rybalkin <ribalkin@gmail.com>
2025-02-19 11:29:24 +0000

    no build test 12

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ rm -rf snapd
 wget http://apps.syncloud.org/apps/"${SNAPD}" --progress=dot:giga
 tar xzvf "${SNAPD}"
 ./snapd/install.sh
-while ! snap install platform; do
-  echo "retry"
-  sleep 10
-done
+#while ! snap install platform; do
+#  echo "retry"
+#  sleep 10
+#done
